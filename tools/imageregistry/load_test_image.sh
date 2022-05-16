689b42b099c44625b5cf31418906b88c9d56ea49
akihikokuroda <akuroda@us.ibm.com>
2022-05-16 20:12:11 +0000

    move registry pod to rukpak-e2e
    Signed-off-by: akihikokuroda <akuroda@us.ibm.com>

--- tools/imageregistry/load_test_image.sh
+++ tools/imageregistry/load_test_image.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-NODE_PORT=$(kubectl get service docker-registry -n rukpak-system  -o jsonpath='{ .spec.ports[0].nodePort }')
+export REGISTRY_NAME="docker-registry"
+export REGISTRY_NAMESPACE=rukpak-e2e
+export DNS_NAME=$REGISTRY_NAME.$REGISTRY_NAMESPACE.svc.cluster.local
 
 # push test bundle image into in-cluster docker registry
-kubectl exec nerdctl -n rukpak-system -- sh -c 'nerdctl login -u myuser -p mypasswd docker-registry:5000 --insecure-registry'
+kubectl exec nerdctl -n $REGISTRY_NAMESPACE -- sh -c "nerdctl login -u myuser -p mypasswd $DNS_NAME:5000 --insecure-registry"
 
 for x in $(docker images --format "{{.Repository}}:{{.Tag}}" | grep testdata); do
-    kubectl exec nerdctl -n rukpak-system -- sh -c "nerdctl -n k8s.io tag $x docker-registry:5000${x##testdata}"
-    kubectl exec nerdctl -n rukpak-system -- sh -c "nerdctl -n k8s.io push docker-registry:5000${x##testdata} --insecure-registry"
+    kubectl exec nerdctl -n $REGISTRY_NAMESPACE -- sh -c "nerdctl -n k8s.io tag $x $DNS_NAME:5000${x##testdata}"
+    kubectl exec nerdctl -n $REGISTRY_NAMESPACE -- sh -c "nerdctl -n k8s.io push $DNS_NAME:5000${x##testdata} --insecure-registry"
 done
 
--- tools/imageregistry/setup_imageregistry.sh
+++ tools/imageregistry/setup_imageregistry.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
 
+export REGISTRY_NAME="docker-registry"
+export REGISTRY_NAMESPACE=rukpak-e2e
+kubectl create ns $REGISTRY_NAMESPACE || true
 kubectl create ns rukpak-system || true
 
 # create self-signed certificate for registry server
+export DNS_NAME=$REGISTRY_NAME.$REGISTRY_NAMESPACE.svc.cluster.local
+
 
 mkdir -p /tmp/var/imageregistry/certs
-openssl req -x509 -newkey rsa:4096 -days 365 -nodes -sha256 -keyout /tmp/var/imageregistry/certs/tls.key -out /tmp/var/imageregistry/certs/tls.crt -subj "/CN=docker-registry" -addext "subjectAltName = DNS:docker-registry"
-kubectl create secret tls certs-secret --cert=/tmp/var/imageregistry/certs/tls.crt --key=/tmp/var/imageregistry/certs/tls.key -n rukpak-system
-kubectl create configmap trusted-ca -n kube-system --from-file=ca.crt=/tmp/var/imageregistry/certs/tls.crt
+openssl req -x509 -newkey rsa:4096 -days 365 -nodes -sha256 -keyout /tmp/var/imageregistry/certs/tls.key -out /tmp/var/imageregistry/certs/tls.crt -subj "/CN=$DNS_NAME" -addext "subjectAltName = DNS:$DNS_NAME"
+kubectl create secret tls certs-secret --cert=/tmp/var/imageregistry/certs/tls.crt --key=/tmp/var/imageregistry/certs/tls.key -n $REGISTRY_NAMESPACE
+kubectl create configmap trusted-ca -n $REGISTRY_NAMESPACE --from-file=ca.crt=/tmp/var/imageregistry/certs/tls.crt
 
 # create image registry service
-kubectl apply -f tools/imageregistry/service.yaml -n rukpak-system
+kubectl apply -f tools/imageregistry/service.yaml -n $REGISTRY_NAMESPACE
 
 # set local variables
-export REGISTRY_NAME="docker-registry"
-export REGISTRY_IP=$(kubectl get service docker-registry -n rukpak-system -o jsonpath='{ .spec.clusterIP }')
+export REGISTRY_IP=$(kubectl get service $REGISTRY_NAME -n $REGISTRY_NAMESPACE -o jsonpath='{ .spec.clusterIP }')
 export REGISTRY_PORT=5000
-export NODE_PORT=$(kubectl get service docker-registry -n rukpak-system  -o jsonpath='{ .spec.ports[0].nodePort }')
-export NODE_IP=$(kubectl get node kind-control-plane -n rukpak-system -o jsonpath='{ .status.addresses[?(@.type=="InternalIP")].address }')
+export NODE_PORT=$(kubectl get service $REGISTRY_NAME -n $REGISTRY_NAMESPACE  -o jsonpath='{ .spec.ports[0].nodePort }')
+export NODE_IP=$(kubectl get node kind-control-plane -n $REGISTRY_NAMESPACE -o jsonpath='{ .status.addresses[?(@.type=="InternalIP")].address }')
 
 #### Warning - Update test host configuration ####
 # The following updates make "docker push docker-registry$NODE_PORT/image name:tag" work on the host system
@@ -27,20 +31,21 @@ export NODE_IP=$(kubectl get node kind-control-plane -n rukpak-system -o jsonpat
 # cp /tmp/var/imageregistry/certs/tls.crt /etc/docker/certs.d/$REGISTRY_NAME:$NODE_PORT/ca.crt
 
 # Add ca certificate to Node
-kubectl apply -f tools/imageregistry/daemonset.yaml
+kubectl apply -f tools/imageregistry/daemonset.yaml -n $REGISTRY_NAMESPACE
 
 # Add an entry in /etc/hosts of Node
-docker exec $(docker ps | grep 'kind-control-plane' | cut -c 1-12) sh -c "/usr/bin/echo $REGISTRY_IP $REGISTRY_NAME >>/etc/hosts"
+docker exec $(docker ps | grep 'kind-control-plane' | cut -c 1-12) sh -c "/usr/bin/echo $REGISTRY_IP $DNS_NAME >>/etc/hosts"
 
 sleep 5
 # create image registry pod
-kubectl apply -f tools/imageregistry/registry.yaml -n rukpak-system
+kubectl apply -f tools/imageregistry/registry.yaml -n $REGISTRY_NAMESPACE
 
 # create image upload  pod
-kubectl apply -f tools/imageregistry/nerdctl.yaml -n rukpak-system
+kubectl apply -f tools/imageregistry/nerdctl.yaml -n $REGISTRY_NAMESPACE
 
-# create imagePull secret
-kubectl create secret docker-registry registrysecret --docker-server=docker-registry:5000 --docker-username="myuser" --docker-password="mypasswd" --docker-email="email@foo..com" -n rukpak-system
+# create imagePull secret for host system
+kubectl create secret docker-registry registrysecret --docker-server=$DNS_NAME:5000 --docker-username="myuser" --docker-password="mypasswd" --docker-email="email@foo..com" -n rukpak-system
+kubectl create secret docker-registry registrysecret --docker-server=$DNS_NAME:5000 --docker-username="myuser" --docker-password="mypasswd" --docker-email="email@foo..com" -n $REGISTRY_NAMESPACE
 export IMAGE_PULL_RECRET="registrysecret"
 
 echo #### Valiables ####
